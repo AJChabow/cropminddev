0d29bedc6280b97f39db1c6f384b622320946f72
Artur Chabowski <a.j.chabowski@gmail.com>
2024-04-13 11:39:27 +0200

    mainly the home page changes

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,66 +1,52 @@
 import React from 'react';
+import './AboutPage.css';
 
 function AboutPage() {
   return (
-    <div className='max-w-[800px] w-full mx-auto flex flex-col'>
-<div className='text-center'>
-      <h1 className='text-3xl my-5'><strong>Mission</strong></h1>
+    <div className='font-poppins'>
+      <div className='max-w-[1200px] w-full mx-auto flex flex-col'>
+        <div className='text-center'>
+          <h1 className='text-3xl text-green-700'>
+            <strong>Developing an open platform, making farming better for everyone.</strong>
+          </h1>
+          <p className='my-5 font-semibold'>
+            We aim to simplify complex decisions, while linking farmers to the latest agricultural technology, research and robotic solutions. Our collaborative ecosystem ensures that the future of farming is sustainable, efficient, and accessible to all.
+          </p>
+        </div>
+        <div className='flex md:flex-row responsive-column'>
+        <img className='rounded-r-lg my-3 ' src={process.env.PUBLIC_URL + "/images/farmer.png"} alt='showing a farmer' style={{ width: '100%', maxWidth: '800px' }}></img>
+          <div className='text-block my-3 ml-16'>
 
-      <p className='text-2xl text-green-700'>
-        <strong>Developing an open platform, making farming better for everyone.</strong>
-      </p>
-      <p className='my-5'>
-      We aim to simplify complex decisions, while linking farmers to the latest agricultural technology, research and robotic solutions. Our collaborative ecosystem ensures that the future of farming is sustainable, efficient, and accessible to all.
-      </p>
+            <strong className='text-green-700 text-xl font-semibold'>An ecosystem for all, with farmers at the heart</strong>
+            <p className='text-lg'>
+              We are committed to nurturing an ecosystem that uplifts and empowers everyone dedicated to enriching agriculture, placing farmers at the core of our mission. Our platform aims to bridge the gap between private companies and public institutes, facilitating the provision of crucial data, models, and robotic solutions in a standardized manner, directly benefiting farmers.
+            </p>
+          </div>
+          
+        </div>
+        <div className='flex md:flex-row responsive-column'>
+        <div className='text-block my-3 mr-16'>
+            <strong className='text-green-700 text-2xl font-semibold'>The first farm is our own</strong>
+            <p className='text-lg'>
+              Our journey began in the fields of our own farm, confronting firsthand the challenges that farmers face daily. 'The first farm we are empowering is our own' reflects our commitment to creating solutions rooted in actual agricultural experiences. This endeavour has evolved into a platform inspired by real farm challenges, aimed at empowering farmers with innovation and sustainability from the ground up.
+            </p>
+          </div>
+          <img className='rounded-l-lg my-3' src={process.env.PUBLIC_URL + "/images/sunrise.jpeg"} alt='showing a farmer' style={{ width: '100%', maxWidth: '800px' }}></img>
+        </div>
+
+        <div className='flex justify-center flex-nowrap md:flex-row responsive-column '>
+          <div className='text-block my-3 mx-12'>
+            <strong> Artur Chabowski</strong>
+            <img className='rounded-lg my-2' src={process.env.PUBLIC_URL + "/images/art_headshot.jpeg"} alt='showing Artur Chabowski' style={{ width: '100%', maxWidth: '300px' }}></img>
+          </div>
+          <div className='text-block my-3 mx-12'>
+            <strong> Tobias Nolte</strong>
+            <img className='rounded-lg my-2' src={process.env.PUBLIC_URL + "/images/tob_headshot.jpeg"} alt='showing Tobias Nolte' style={{ width: '100%', maxWidth: '300px' }}></img>
+          </div>
+        </div>
+        
+      </div>
     </div>
-    <div className='flex'>
-      <div className='w-1/2 mr-8'>
-      <img src = {process.env.PUBLIC_URL + "/images/farmer.png"} alt='showing a farmer'></img>
-      </div>
-      <div className='w-1/2 mr-8'>
-        <strong className='my-3'>Our Focus</strong> <br></br>
-        <strong className='text-green-700'> An ecosystem for all, with farmers at the heart </strong>
-        <p>
-        We are committed to nurturing an ecosystem that uplifts and empowers everyone dedicated to enriching agriculture, placing farmers at the core of our mission. Our platform aims to bridge the gap between private companies and public institutes, facilitating the provision of crucial data, models, and robotic solutions in a standardised manner, directly benefiting farmers.
-        </p>
-
-
-      </div>
-    </div>
-    <br></br>
-    <div className='flex'>
-      
-      <div className='w-1/2 mr-8'>
-        <strong className='my-3'>Founding Story</strong> <br></br>
-        <strong className='text-green-700'> The first farm we are empowering is our own </strong>
-        <p>
-        Our journey began in the fields of our own farm, confronting firsthand the challenges that farmers face daily. 'The first farm we are empowering is our own' reflects our commitment to creating solutions rooted in actual agricultural experiences. This endeavour has evolved into a platform inspired by real farm challenges, aimed at empowering farmers with innovation and sustainability from the ground up.
-        </p>
-
-
-      </div>
-      <div className='w-1/2 mr-8'>
-      <img src = {process.env.PUBLIC_URL + "/images/sunrise.jpeg"} alt='showing a farmer'></img>
-      </div>
-    </div>
-    <br></br>
-    <br></br>
-
-    <div className='flex m-5'>
-      <div className='w-1/2 mr-4'>
-      <img className='my-2' src = {process.env.PUBLIC_URL + "/images/art_headshot.jpeg"} alt='showing a farmer'></img>
-      <strong> Artur Chabowski</strong>
-      </div>
-      <div className='w-1/2 mr-4'>
-      <img className='my-2' src = {process.env.PUBLIC_URL + "/images/tob_headshot.jpeg"} alt='showing a farmer'></img>
-      <strong> Tobias Nolte</strong>
-      </div>
-
-    </div>
-    </div>
-
-
-    
   );
 }
 
--- src/CompanyPage.jsx
+++ src/CompanyPage.jsx
@@ -6,27 +6,11 @@ import AnimatedBorderCTA from './AnimatedBorderCTA';
 
 
 
-const ServiceCard = ({ icon, title, description }) => {
-  return (
-    <div className="fill rounded-lg overflow-hidden shadow-lg p-8 m-6 border-2 border-green-500">
-      <div className="flex items-center">
-        <img className="w-10 h-10 mr-4" src={icon} alt="Service icon" />
-        <div className="flex flex-col justify-between">
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{description}</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const GrayBar = () => {
   return (
     <div className="w-3/5 mx-auto my-8 bg-gray-300 h-2 rounded-full"></div>
   );
 };
-
-// Adjust PageCard component for mobile
 const PageCard = ({ icon, title, description }) => {
   const [isVisible, setIsVisible] = useState(false);
   const cardRef = useRef(null);
@@ -38,31 +22,31 @@ const PageCard = ({ icon, title, description }) => {
           setIsVisible(true);
         }
       });
-    }, { threshold: 0.1 }); // Adjust threshold value as needed
+    }, { threshold: 0.1 });
 
     if (cardRef.current) {
       observer.observe(cardRef.current);
     }
 
     return () => {
-      if(cardRef.current) {
+      if (cardRef.current) {
         observer.unobserve(cardRef.current);
       }
     };
   }, []);
 
   return (
-    <div ref={cardRef} className={`transition-opacity duration-1000 ease-in ${isVisible ? 'opacity-100' : 'opacity-0'} flex flex-col md:flex-row items-center justify-center mx-4 md:mx-16 my-8 h-auto md:h-[80vh]`}>
-      <div className='md:w-1/3 mb-4 md:mb-0'>
+    <div ref={cardRef} className={`PageCard my-28 transition-opacity duration-1000 ease-in ${isVisible ? 'opacity-100' : 'opacity-0'} flex items-center justify-center mx-auto my-8`} style={{ maxWidth: '1200px' }}>
+      <div className='text-container flex-1 my-12 mr-10' style={{ flexBasis: '40%', maxWidth: '480px' }}>
         <h1 className='font-bold text-green-800 text-3xl md:text-4xl'>{title}</h1>
         <p className='text-xl md:text-2xl'>{description}</p>
       </div>
-      <div className='w-full md:w-1/2'>
+      <div className='image-container flex-1' style={{ flexBasis: '60%' }}>
         <img
           src={icon}
           alt='UI of the recommendation system'
           loading="lazy"
-          className="transition-opacity duration-400 ease-in"
+          className="transition-opacity duration-400 ease-in w-full"
         />
       </div>
     </div>
@@ -70,30 +54,77 @@ const PageCard = ({ icon, title, description }) => {
 };
 
 
-const bgImageUrl = `${process.env.PUBLIC_URL}/images/orchard.svg`;
+
 function CompanyPage() {
+  const bgImageUrl = `${process.env.PUBLIC_URL}/images/orchard.svg`;
+  const [backgroundStyle, setBackgroundStyle] = useState({
+    backgroundSize: 'cover', // Default to cover
+    backgroundPosition: 'center center'
+  });
+
+  useEffect(() => {
+    function updateBackgroundSize() {
+      const { innerWidth: width, innerHeight: height } = window;
+
+      // Assuming you want the SVG to fill the screen at 1080p resolution without scaling
+      if (width === 1920 && height === 1080) {
+        setBackgroundStyle({
+          backgroundSize: 'contain', // Ensures the entire image is visible
+          backgroundPosition: 'center center'
+        });
+      } else {
+        setBackgroundStyle({
+          backgroundSize: 'cover', // Covers the entire container, may crop edges
+          backgroundPosition: 'center center'
+        });
+      }
+    }
+
+    window.addEventListener('resize', updateBackgroundSize);
+    updateBackgroundSize(); // Call on mount to set initial size
+
+    return () => {
+      window.removeEventListener('resize', updateBackgroundSize);
+    };
+  }, []);
+
   return (
     <>
+      <div className="h-screen flex items-center justify-center"
+           style={{ position: 'relative', width: '100%', height: '100vh' }}>
+        <div style={{
+          backgroundImage: `url(${bgImageUrl})`,
+          backgroundSize: backgroundStyle.backgroundSize,
+          backgroundPosition: backgroundStyle.backgroundPosition,
+          backgroundRepeat: 'no-repeat',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          opacity: 0.5,
+          filter: 'blur(5px)',
+          zIndex: -1,
+        }}></div>
 
+        <div className="outer-headings text-center m-20" style={{ position: 'relative', zIndex: 1 }}>
+          <h1 className='text-6xl font-semibold font-poppins'>
+            Ideal farming decisions, made easy
+          </h1>
+          <br></br>
+          <h2 className='max-w-[50rem] text-2xl font-poppins'>
+            A wealth of data sources, the latest in agricultural research and technology relevant to your farming decisions
+          </h2>
+          <br></br>
+          <h3 className='max-w-[50rem] text-2xl font-poppins font-bold'>
+            all integrated in one platform
+          </h3>
+        </div>
+      </div>
 
-    <div 
-      style={{ backgroundImage: `url(${bgImageUrl})` }} 
-      className="bg-cover h-screen flex items-center justify-center ">
-        <p className = "outer-headings text-center m-20">
-            <h1 className='text-4xl font-bold max-w-xl font-poppins'>
-            Empower Your Farm and Connect with Innovation, all in one open platform
-            </h1>
-            <br>
-            </br>
-            <h2 className='max-w-[50rem] text-2xl font-poppins'>
-            Access a wealth of relevant data sources, automate and simplify farming decisions, and seamlessly integrate the latest in agricultural research & robotics
-            </h2>
-          </p>
-
-    </div>
   
 
-<PageCard icon = {process.env.PUBLIC_URL + "/images/crecommender.png"}
+<PageCard icon = {process.env.PUBLIC_URL + "/images/databrowser.svg"}
           title = 'Automated Data-Driven Recommendations at Your Fingertips'
           description = 'Unlock precision agriculture with personalized, data-driven recommendations. Guided by comprehensive analysis, make informed decisions to boost yield and conserve valuable resources, and avoid repetitive time-consuming tasks.'
 />
@@ -104,7 +135,7 @@ function CompanyPage() {
 />
 <GrayBar />
 
-<PageCard icon = {process.env.PUBLIC_URL + "/images/for_cherished_broski.png"}
+<PageCard icon = {process.env.PUBLIC_URL + "/images/apps.svg"}
           title = 'Discover Expert Solutions at Your Service'
           description = 'Dive into our Solutions Marketplace, showcasing services, data, and the latest in crop research all provided by experts in their fields. Here, where innovation meets expertise, gain access to agricultural tools designed by industry leaders for unparalleled growth and efficiency.'
 />
@@ -115,41 +146,13 @@ function CompanyPage() {
     <AnimatedBorderCTA>
       <div className="flex flex-col items-center p-8">
         <h2 className="text-center text-3xl font-semibold my-8">Join Our Waitlist</h2>
-        <EmailSignup />
+        <EmailSignup/>
       </div>
+      
     </AnimatedBorderCTA>
   </div>
 </div>
 
-
-        
-      
-<h1 className='font-semibold text-2xl text-center underline font-poppins '>
-    Tools to bridge the gap between research and application.
-    </h1>
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-      <ServiceCard
-        icon= {process.env.PUBLIC_URL + "/images/dronegraphic.png"}
-        title="Manage services for your farm."
-        description=""
-      />
-      <ServiceCard
-        icon= {process.env.PUBLIC_URL + "/images/databrowser.png"}
-        title="Integrate weather forecasts, soil data, satellite data, seamlessly."
-        description=""
-      />
-      <ServiceCard
-        icon= {process.env.PUBLIC_URL + "/images/beaker.png"}
-        title="Access your local institute's best irrigation practices for your crop, and have recommendations built for you."
-        description=""
-      />
-      <ServiceCard
-        icon= {process.env.PUBLIC_URL + "/images/appstore.png"}
-        title="Integrate weather forecasts, soil data, satellite data, seamlessly."
-        description=""
-      />
-      </div>
-      
       </>
  
  );
